41c26caf15d60442d43745b14130f93191942393
Joshua Yorko <joshua.yorko@gmail.com>
2024-04-23 16:55:26 -0400

    Add new Python script

---
+++ JoshBestChatgpt.py
@@ -0,0 +1,10 @@
+# Josh's Best ChatGPT Script
+
+def process_data(data):
+    # Simple function to process data
+    return data.upper()
+
+if __name__ == '__main__':
+    sample_data = 'Hello, this is a test data string.'
+    processed_data = process_data(sample_data)
+    print(processed_data)
